67345a6b74c2790c97606246a3fe8a541f753cee
NullDefault <davisha999@gmail.com>
2019-12-23 18:22:25 -0500

    second part of tutorial complete

--- source/engine.py
+++ source/engine.py
@@ -1,13 +1,23 @@
 import tcod as libtcod
-from source import input_master
+from source import input_master, entity, render_functions
+from source.map_engine.game_map import GameMap
+from source.render_functions import render_all, clear_all
 
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    player = entity.Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = entity.Entity(int(screen_width / 3), int(screen_height / 3), 'G', libtcod.red)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
@@ -15,18 +25,18 @@ def main():
 
     console = libtcod.console_new(screen_width, screen_height)
 
+    game_map = GameMap(map_width, map_height)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(console, libtcod.light_green)
-        libtcod.console_put_char(console, player_x, player_y, "@", libtcod.BKGND_NONE)
-        libtcod.console_blit(console, 0, 0, screen_width, screen_height, 0, 0, 0)
+        render_all(console, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
 
-        libtcod.console_put_char(console, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(console, entities)
 
         action = input_master.handle_keys(key)
 
@@ -36,8 +46,8 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if end_game:
             return True
